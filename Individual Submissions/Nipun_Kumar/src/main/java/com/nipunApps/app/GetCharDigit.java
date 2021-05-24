62d70b46c34bd41866f1a2fe8dcce539f0534777
Nipun Kumar <nipunvirat0@gmail.com>
2021-05-24 16:57:40 +0530

    added the passowrd generator class as a module

---
+++ Individual Submissions/Nipun_Kumar/src/main/java/com/nipunApps/app/GetCharDigit.java
@@ -0,0 +1,12 @@
+package com.nipunApps.app;
+
+public interface GetCharDigit {
+    //    This interface simply provide methods to return all types of letter digit and specialChar you want to use
+    String getDigit();
+
+    String getSpecialChar();
+
+    String lowercase();
+
+    String getUpperCase();
+}
