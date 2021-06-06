532d2f1255ee720a46d6eeb47b36f467d73cbf57
rishab0902 <rishab.davksd@gmail.com>
2021-06-06 11:35:52 +0530

    changed methods from public to private

--- Individual Submissions/Rishab/src/main/java/com/chillyfacts/passgen/App.java
+++ Individual Submissions/Rishab/src/main/java/com/chillyfacts/passgen/App.java
@@ -7,12 +7,12 @@ import java.util.Scanner;
  */
 public class App 
 {
-	public static void printArray(String[] arr) {
+	private static void printArray(String[] arr) {
         for(int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
-	public static char randomCharacter() {
+	private static char randomCharacter() {
         int rand = (int)(Math.random()*62);
         if(rand <= 9) {
             int number = rand + 48;
