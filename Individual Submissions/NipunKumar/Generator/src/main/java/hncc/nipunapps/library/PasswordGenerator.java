adde92df927f43e42e99573d0694ef22ce8cdbba
Nipun <nipunvirat0@gmail.com>
2021-06-21 00:37:00 +0530

    snip add

--- Individual Submissions/NipunKumar/Generator/src/main/java/hncc/nipunapps/library/PasswordGenerator.java
+++ Individual Submissions/NipunKumar/Generator/src/main/java/hncc/nipunapps/library/PasswordGenerator.java
@@ -14,12 +14,14 @@ public class PasswordGenerator implements CharDigits {
         this.length = length;
     }
 
-    // By default this class will use only lower and upper case for password generating
+    // By default this class generates the password with usage all the character
     private Boolean useLower=true, useUpper=true, useDigit=true, useSpecialChar=true;
 
+    // Empty constructor
     public PasswordGenerator() {
     }
 
+    // Constructor which takes all type of char usage boolean
     public PasswordGenerator(Boolean useLower, Boolean useUpper, Boolean useDigit, Boolean useSpecialChar) {
         this.useLower = useLower;
         this.useUpper = useUpper;
@@ -27,6 +29,9 @@ public class PasswordGenerator implements CharDigits {
         this.useSpecialChar = useSpecialChar;
     }
 
+    /* Overriding the Chardigits methods
+     * Any one who use this class must set the chars and digits
+     */
     @Override
     public String getDigit() {
         return "0123456789";
@@ -48,9 +53,13 @@ public class PasswordGenerator implements CharDigits {
         String upperCase = getLowercase().toUpperCase();
         return upperCase;
     }
-
-    ArrayList<String> charChats=new ArrayList<>();
-    public String generatePassword(){
+    // Arraylist which contain all catagories of using in generating the password
+    private  final ArrayList<String> charChats=new ArrayList<>();
+    private String generatePassword(){
+        /* The method which generates the password
+         * If the length is less than 4 method don't generate any password
+         * Method use Random for generating the password
+         */
         if(length<4){
             return "You must create at least 4 digit password";
         }
@@ -62,17 +71,28 @@ public class PasswordGenerator implements CharDigits {
         // Building the password
         String specialChar=getSpecialChar();
         for(int i=0;i<length;i++){
+            /* We add all catagories such as lower case
+             * upper case , digits and Special char string in a Arraylist
+             */
             String charChat;
-            if(useUpper && i==0){
-                charChat=charChats.get(i);
-            }
+            /* The random simply takes any of cases string and
+             * put them in charChat string since this work
+             * is done by random so every time in charchat string
+             * different type of case will stored.
+             */
             charChat= charChats.get(random.nextInt(charChats.size()));
+            /* We again use random for getting
+             * a random character from charChat string .
+             */
             password[i]=charChat.charAt(random.nextInt(charChat.length()));
         }
         // Returning the password
         return new String(password);
     }
     private void getCharUses(){
+        /* This method set the Arraylist
+
+         */
         charChats.clear();
         if(useUpper){
             charChats.add(getUpperCase());
@@ -87,6 +107,10 @@ public class PasswordGenerator implements CharDigits {
             charChats.add(getSpecialChar());
         }
     }
+
+    /* User can set all the cases in runtime
+     * with the help of these setters
+     */
     public void setUseLower(Boolean useLower) {
         this.useLower = useLower;
     }
