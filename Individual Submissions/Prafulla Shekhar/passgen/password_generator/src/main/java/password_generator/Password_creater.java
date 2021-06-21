67dbfccd2583ad4bce6751b726812f693093bfc4
Prafulla Shekhar <prafullashekhar@gmail.com>
2021-06-21 11:27:41 +0530

    added automatic copy of generated password

--- Individual Submissions/Prafulla Shekhar/passgen/password_generator/src/main/java/password_generator/Password_creater.java
+++ Individual Submissions/Prafulla Shekhar/passgen/password_generator/src/main/java/password_generator/Password_creater.java
@@ -7,14 +7,14 @@ public class Password_creater {
 	// function to generate password using characters from char_box
 	private static char[] generator(int password_length) {
 
-		String char_box = "!@#$%^&*()_+1234567890-=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String char_box = "!@#$%^&*?_+1234567890-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 		Random random = new Random();
 
 		char[] pass = new char[password_length];
 
 		for (int i = 0; i < password_length; i++) {
-			pass[i] = char_box.charAt(random.nextInt(76));
+			pass[i] = char_box.charAt(random.nextInt(74));
 		}
 
 		return pass;
--- Individual Submissions/Prafulla Shekhar/passgen/password_runner/src/main/java/password_runner/Runner.java
+++ Individual Submissions/Prafulla Shekhar/passgen/password_runner/src/main/java/password_runner/Runner.java
@@ -1,5 +1,8 @@
 package password_runner;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
 
 import password_generator.Password_creater;
@@ -9,42 +12,58 @@ public class Runner {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
+		// n is the length of the password
 		int n = 8;
 
 		try {
 
-			if (args[0].isEmpty()) {
-				n = 8;
-			} else {
+			if (!args[0].isEmpty()) {
+
 				String str = args[0];
 				String strlen = str.substring(1);
 				n = Integer.parseInt(strlen);
+
+				/*
+				 * Accepting password length greater than 4 and less than 300 n /= 0 will throw
+				 * ArithmeticException and it will call help
+				 */
+
+				if (n < 4) {
+					System.out.println("** it's unsecure to use password less than 4 digits **");
+					n /= 0;
+				} else if (n > 300) {
+					System.out.println("** to large password **");
+					n /= 0;
+				}
 			}
 
-			if (n < 4)
-				n /= 0;
-
-			String password = Password_creater.getPassword(n);
-			System.out.println("Password: " + password);
-
-		} catch (ArithmeticException e) {
-
-			System.out.println("** minimum password length supported is 4 **");
-			help();
+			printpass(n);
 
 		} catch (ArrayIndexOutOfBoundsException e) {
-
-			String password = Password_creater.getPassword(8);
-			System.out.println("Password: " + password);
+			printpass(8);
 
 		} catch (Exception e) {
-
 			help();
-
 		}
 
 	}
 
+	// function to print password and get it copied in clipboard
+	public static void printpass(int n) {
+
+		// generates the random password
+		String password = Password_creater.getPassword(n);
+
+		StringSelection stringSelection = new StringSelection(password);
+
+		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+		clipboard.setContents(stringSelection, null);
+
+		System.out.println("Password: " + password);
+
+	}
+
+	// function to print the help statements
 	public static void help() {
 
 		System.out.println("--help--Input format should be as below");
@@ -52,6 +71,7 @@ public class Runner {
 		System.out.println("java -jar passgen.jar -l");
 		System.out.println("(where l= length of password)");
 		System.out.println();
+
 	}
 
 }
