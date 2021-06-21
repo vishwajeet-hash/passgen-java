c6c20a05f6f8ea2d1bfc0dfd14284d033d526921
vishwajeet singh <v.k.singh220701@gmail.com>
2021-06-21 16:03:14 +0530

    Added copy to clipboard feature

--- Individual Submissions/vishwajeet/src/main/java/com/chillyfacts/passgen/App.java
+++ Individual Submissions/vishwajeet/src/main/java/com/chillyfacts/passgen/App.java
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
 package com.chillyfacts.passgen;
-
+import java.awt.datatransfer.StringSelection;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
 import java.util.*;
 //import java.io.*;
 import java.util.concurrent.TimeUnit;
@@ -25,6 +26,10 @@ public class App
         {
              newpass[i]=mix.charAt(variable.nextInt(mix.length()) );
         }
+        String myString = String. valueOf(newpass);
+        StringSelection stringSelection = new StringSelection(myString);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
         return newpass;
 
     }
@@ -85,75 +90,3 @@ public class App
         System.out.println("It will generate a Password of length 6");
     }
 }
-=======
-package com.chillyfacts.passgen;
-
-import java.util.*;
-//import java.io.*;
-import java.util.concurrent.TimeUnit;
-public class App 
-{
-    public static void main( String[] args ) throws InterruptedException
-    {
-
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Press 1 for generating password :");
-        System.out.println("Press 2 for setting length. Length should not be less than 4.");
-        int userchoice =sc.nextInt();
-        switch(userchoice)
-        {
-        case 1:
-        {
-            int length=8;
-            System.out.println(passgen(length));
-            break;
-        }
-        case 2:
-        {
-        	int length;
-        	do {
-            System.out.println("Enter length:");
-            length=sc.nextInt();
-            
-            if(length<4) {
-            	System.out.println("wrong input.Please enter length greater than 4");
-            }
-        	}
-            while(length<4);
-            	
-            
-            System.out.println(passgen(length));
-            break;
-        }
-        default:
-            System.out.println("wrong input");
-            main(args);
-        
-        }
-        sc.close();
-    }
-    private static char[] passgen(int l) throws InterruptedException
-    {
-        System.out.println("Generating password using random...");
-        TimeUnit.SECONDS.sleep(1);
-        System.out.println("Your new password is:");
-        String special="!@#$%^&*_=+-/.?<>";
-        String capital="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String small="abcdefghijklmnopqrstuvwxyz";
-        String digits="0123456789";
-
-        String mix=special + capital +  small + digits;
-        
-        //using random method
-        Random variable=new Random();
-       char[] newpass=new char[l];
-        for(int i=0;i<l;i++)
-        {
-             newpass[i]=mix.charAt(variable.nextInt(mix.length()) );
-        }
-        return newpass;
-
-    }
-    
-}
->>>>>>> 46c8978f655eb7e33f272951c12d27795204d0d7
